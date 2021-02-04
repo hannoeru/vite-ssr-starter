9b2197a87d61290fb9eb603aeabb3c7317ac2927
hanlee <me@hanlee.co>
2021-02-04 19:59:33 +0900

    feat: add vite icon & components plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { UserConfig } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue'
+import VuePlugin from '@vitejs/plugin-vue'
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
+import ViteComponents from 'vite-plugin-components'
 
 const config: UserConfig = {
-  plugins: [vuePlugin()],
+  plugins: [
+    VuePlugin(),
+    // https://github.com/antfu/vite-plugin-components
+    ViteComponents({
+      customComponentResolvers: [
+        ViteIconsResolver({
+          componentPrefix: '',
+        }),
+      ],
+    }),
+    // https://github.com/antfu/vite-plugin-icons
+    ViteIcons({
+      scale: 1.1,
+      defaultStyle: 'vertical-align: middle;',
+    }),
+  ],
   build: {
-    minify: false
-  }
+    minify: false,
+  },
 }
 
 export default config
