6c1cede320ef812292e760082f067eb51f1eb243
hanlee <me@hanlee.co>
2021-02-06 16:21:01 +0900

    feat: use import syntax

--- prerender.ts
+++ prerender.ts
@@ -1,16 +1,11 @@
 // Pre-render the app into static HTML.
 // run `yarn generate` and then `dist/static` can be served as a static site.
-/* eslint-disable @typescript-eslint/no-var-requires */
 
 import fs from 'fs'
 import path from 'path'
 import fg from 'fast-glob'
 
 const toAbsolute = (p: string) => path.resolve(__dirname, p)
-
-const manifest = require('./dist/static/ssr-manifest.json')
-const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
-const { render } = require('./dist/server/entry-server.js')
 function ensureDirExist(filePath: string) {
   const dirname = path.dirname(filePath)
   if (fs.existsSync(dirname))
@@ -21,8 +16,16 @@ function ensureDirExist(filePath: string) {
 }
 
 export async function build() {
+  // @ts-ignore
+  const manifest = await import('./dist/static/ssr-manifest.json')
+  const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
+
+  // @ts-ignore
+  const { render } = await import('./dist/server/entry-server.js')
+
   // determine routes to pre-render from src/pages
   const files = await fg('**/*.{vue,md}', { cwd: path.resolve(process.cwd(), 'src/pages') })
+
   const routesToPrerender = files
     .filter(i => !i.includes('['))
     .map((file) => {
