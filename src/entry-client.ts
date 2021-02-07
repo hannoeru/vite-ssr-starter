4cf13f134da7a355c68a1fe8e0dbfc38b054c067
hanlee <me@hanlee.co>
2021-02-07 23:42:30 +0900

    chore: update build script

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -4,5 +4,5 @@ const { app, router } = createApp()
 
 // wait until router is ready before mounting to ensure hydration match
 router.isReady().then(() => {
-  app.mount('#app')
+  app.mount('#app', true)
 })
